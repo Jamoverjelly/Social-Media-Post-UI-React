c4665ab959a3a27de9df9d007174aadd72c27ad8
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-05-15 11:31:23 -0400

    Initial commit

---
+++ src/components/card-body/card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardHeader from "../card-header/cardHeader";
+
+function Card() {
+  return (
+    <div className="card">
+      <CardHeader />
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/card-header/cardHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CardHeader() {
+  return (
+    <div className="card-header">
+      <p>Hello from cardHeader.js</p>
+    </div>
+  );
+}
+
+export default CardHeader;
